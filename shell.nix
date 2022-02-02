038d338470db7ff551b4496c9dacfdb64a6ac12e
grdddj <jiri.musil06@seznam.cz>
2022-02-02 21:42:22 +0100

    nix-shell and poetry

---
+++ shell.nix
@@ -0,0 +1,22 @@
+let
+  # the last successful build of nixpkgs-unstable as of 2021-11-18
+  nixpkgsCommit = "7fad01d9d5a3f82081c00fb57918d64145dc904c";
+  nixpkgsSha256 = "0g0jn8cp1f3zgs7xk2xb2vwa44gb98qlp7k0dvigs0zh163c2kim";
+
+  nixpkgsUrl = "https://github.com/NixOS/nixpkgs/archive/${nixpkgsCommit}.tar.gz";
+in
+with import
+  (builtins.fetchTarball {
+    url = nixpkgsUrl;
+    sha256 = nixpkgsSha256;
+  }) { };
+stdenv.mkDerivation {
+  name = "programming-bitcoin";
+  buildInputs = [
+    python39
+    poetry
+    git
+  ];
+  # NIX_PATH needed for autoPatchelfHook nix-shells in download.sh scripts
+  NIX_PATH = "nixpkgs=${nixpkgsUrl}";
+}
